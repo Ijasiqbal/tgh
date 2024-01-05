78efc6ce15267469e0ea517144ae6caf0d62f38e
Ijas Iqbal <ijasiqbal4@gmail.com>
2024-01-05 17:16:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="base">
+      <div className="tab">
+        <button>Home</button>
+        <button>Bookmarks</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
